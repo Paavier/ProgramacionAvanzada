71635e46dc8803925b2d54ee3eb90ae875cbb76b
Paavier <pedrojavierof29@gmail.com>
2025-04-30 18:35:55 +0200

    Add files via upload

---
+++ pa1p42/src/main/java/org/cuentapalabras/PruebaPalabraEnTexto.java
@@ -0,0 +1,19 @@
+package org.cuentapalabras;
+
+public class PruebaPalabraEnTexto {
+
+     public static void main (String[] args){
+          PalabraEnTexto palabra1 = new PalabraEnTexto("gorra");
+          PalabraEnTexto palabra2 = new PalabraEnTexto("GORRA");
+
+          palabra1.incrementa();
+          System.out.println("Palabra 1 = " + palabra1);
+          System.out.println("Palabra 2 = " + palabra2);
+
+          if(palabra1.equals(palabra2)){
+               System.out.println("Las palabras son iguales");
+          }else{
+               System.out.println("Las palabras no son iguales");
+          }
+     }
+}
